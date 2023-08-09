b4d85bf970970deaf1e4dbe93297fd452cbfb181
Daisy Viscaya <daisyreeviscaya@gmail.com>
2023-08-09 22:47:51 +0800

    Create organizer to collate the steps in determining the best bundle combination for each input

---
+++ app/interactors/bundles/organizers/calculate.rb
@@ -0,0 +1,22 @@
+"""
+Bundles::Organizers::Calculate.call(
+  bundles_input: '10 IMG 15 flac 13 VID'
+)
+"""
+
+module Bundles
+  module Organizers
+    class Calculate < BaseInteractor
+      include Interactor::Organizer
+
+      expects do
+        required(:bundles_input).filled
+      end
+
+      organize Bundles::Validate,
+               Bundles::Quantify,
+               Bundles::Prepare,
+               Bundles::Print
+    end
+  end
+end
